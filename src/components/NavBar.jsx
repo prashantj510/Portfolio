228eef020b8bab8745f826afacccf137f4b39439
Prashant jha <prashantj510@gmail.com>
2023-08-23 16:53:03 +0530

    null

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -138,7 +138,7 @@ const NavBar = () => {
       {isOpen ? (
         <motion.div
           initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
-          animate={{scale:1,opacity:1}}
+          animate={{ scale: 1, opacity: 1 }}
           className=" min-w-[70vw] flex  z-30 flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
         >
           <nav className=" flex items-center justify-center flex-col">
